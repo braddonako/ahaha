2c90c65ccfad266fde4aeba2282d2b50f2671fc6
Brad Donakowski <braddonako@gmail.com>
2020-04-01 09:51:27 -0600

    Added a createElement for an iframe with jquery

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 console.log('check check, we are working');
 
-document.getElementById('btn').addEventListener('click', function(){
-    document.getElementById('hey').innerHTML = 'A HA HA'
-    
-    console.log('click')
-}, false)
+// document.getElementById('btn').addEventListener('click', function(){
+//     document.getElementById('hey').innerHTML = 'A HA HA'
+
+//     console.log('click')
+// }, false)
+
+
+$('#btn').click(function(){
+    $("<iframe />", {
+        width: "560",
+        height: "315",
+        src: "https://www.youtube.com/embed/aVciLqAK4vk",
+        frameborder: "0",
+        allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
+        allowfullscreen
+    }).appendTo("body")
+    console.log('click click')
+})
+
 
