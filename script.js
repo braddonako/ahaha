b27a1a5a3544709f4150e3b647cfca6e47a9ea20
Brad Donakowski <braddonako@gmail.com>
2020-04-01 09:01:29 -0600

    Added npm install

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ console.log('check check, we are working');
 
 document.getElementById('btn').addEventListener('click', function(){
     document.getElementById('hey').innerHTML = 'A HA HA'
+    
+    console.log('click')
 }, false)
+
