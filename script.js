391674e8fd25427a5112fd2dc630ac5c2f820103
Brad Donakowski <braddonako@gmail.com>
2020-04-05 08:37:25 -0600

    everything is working fine. setTimeout now 11500

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ $('#btn').click(function(){
                class: "giphy-embed",
                display: 'block'
            }).appendTo('.video')
-    }, 9500);
+    }, 11500);
 });
       
 
@@ -137,7 +137,7 @@ $('.txtb').on('keyup', function(e){
                                             d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" />
                                         <path fill-rule="evenodd"
                                             d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                                            clip-rule="evenodd" />
+                                            clip-rule="evenodsd" />
                                         </svg>`).click(function(){
                                             let p = $(this).parent();
                                             p.fadeOut(function(){
